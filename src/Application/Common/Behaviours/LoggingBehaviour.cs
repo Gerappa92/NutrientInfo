8bd1bcd86fbd1c4774880f042958a1458b4ee8d6
Krzysztof Juszcze <krzjus@gmail.com>
2021-10-06 21:24:37 +0200

    added logging behaviour

---
+++ src/Application/Common/Behaviours/LoggingBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Common.Behaviours
+{
+    public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest>
+    {
+        private readonly ILogger _logger;
+
+        public LoggingBehaviour(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var requestName = request.GetType().Name;
+            _logger.LogInformation("Nutrient-Info request: {name} {request}", requestName, request);
+            return Task.CompletedTask;
+        }
+    }
+}
