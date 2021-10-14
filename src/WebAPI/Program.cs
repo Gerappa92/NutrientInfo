c42d6261af28594236aaee33a2fc104d45728940
Krzysztof Juszcze <krzjus@gmail.com>
2021-10-14 23:16:46 +0200

    added azure key vault

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -1,11 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System;
 
 namespace WebAPI
 {
@@ -18,6 +17,17 @@ namespace WebAPI
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                if (context.HostingEnvironment.IsProduction())
+                {
+                    var builtConfig = config.Build();
+                    var secretClient = new SecretClient(
+                        new Uri($"https://{builtConfig["KeyVaultName"]}.vault.azure.net/"),
+                        new DefaultAzureCredential());
+                    config.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
+                }
+            })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
