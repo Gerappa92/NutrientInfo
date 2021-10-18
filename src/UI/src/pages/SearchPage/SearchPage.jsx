507aff485e419f30856ac1d1c79bbc4c17baa640
Krzysztof Juszcze <Krzysztof_Juszcze@epam.com>
2021-10-18 13:10:20 +0200

    updated SearchPage

--- src/UI/src/pages/SearchPage/SearchPage.jsx
+++ src/UI/src/pages/SearchPage/SearchPage.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { Input, Pagination, Empty } from "antd";
-import FoodNutrients from "../../components/FoodNutrients/FoodNutrients";
+import { GenericList } from "../../components/GenericList/GenericList";
 import "./SearchPage.css";
+import { FoodNutrientsListItem } from "../../components/FoodNutrientsListItem/FoodNutrientsListItem";
 
 const { Search } = Input;
 
@@ -55,71 +56,12 @@ function SearchPage() {
     setTableLoading(false);
   };
 
-  const getBasicNutrients = (nutrients) => {
-    let basic = nutrients
-      .filter((n) => bacisNutrientsNumbers.includes(n.number))
-      .map((n) => ({
-        name: n.name,
-        value: n.value + n.unitName,
-      }));
-    let fat = getFats(nutrients);
-    if (fat) {
-      basic.push(fat);
-    }
-    return basic;
-  };
-
-  const getVitamins = (nutrients) => {
-    return nutrients
-      .filter((n) => n.name.includes("Vitamin"))
-      .map((n, i) => ({
-        name: n.name,
-        value: n.value + n.unitName,
-      }));
-  };
-
-  const getElseNutrients = (nutrients) => {
-    let elseNutrients = nutrients
-      .filter(
-        (n) =>
-          !n.name.includes("Vitamin") &&
-          !bacisNutrientsNumbers.includes(n.number) &&
-          !fattyAcidsNumbers.includes(n.number) &&
-          n.number !== fatNumber
-      )
-      .map((n, i) => ({
-        name: n.name,
-        value: n.value + n.unitName,
-      }));
-
-    return elseNutrients;
-  };
-
-  const getFats = (nutrients) => {
-    let fat = nutrients.find((n) => n.number === fatNumber);
-    if (!fat) {
-      return null;
-    }
-    let fattyAcids = nutrients
-      .filter((n) => fattyAcidsNumbers.includes(n.number))
-      .map((n) => ({
-        name: n.name,
-        value: n.value + n.unitName,
-      }));
-    return {
-      name: fat.name,
-      value: fat.value + fat.unitName,
-      children: fattyAcids,
-      key: fat.name,
-    };
-  };
-
   return (
-    <div className='search-page-container'>
+    <div className="search-page-container">
       <div>
         <Search
-          className='food-search'
-          placeholder='Banana'
+          className="food-search"
+          placeholder="Banana"
           onSearch={onSearch}
           enterButton
           allowClear
@@ -131,19 +73,11 @@ function SearchPage() {
           onChange={onPageChange}
           onShowSizeChange={onShowSizeChange}
         />
-        <div className='food-nutrients'>
-          {data.foods.map((f, i) => (
-            <FoodNutrients
-              index={i}
-              description={f.description}
-              brand={f.brandName}
-              source={f.source}
-              nutrients={getBasicNutrients(f.nutrients)}
-              vitamins={getVitamins(f.nutrients)}
-              elseNutrients={getElseNutrients(f.nutrients)}
-              loading={tableLoading}></FoodNutrients>
-          ))}
-        </div>
+        <GenericList
+          items={data.foods}
+          resourceName="food"
+          itemComponent={FoodNutrientsListItem}
+        ></GenericList>
         {data.foods.length === 0 && (
           <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
         )}
@@ -158,23 +92,5 @@ function SearchPage() {
     </div>
   );
 }
-
-const bacisNutrientsNumbers = [
-  208, //Energy
-  203, //Protein
-  205, //Carbohydrates
-  269, //Sugar
-  291, //Fiber
-  307, //Sodium 2.54x Salt
-];
-
-const fatNumber = "204"; //Fat
-
-const fattyAcidsNumbers = [
-  605, //Fatty acids, total trans
-  606, //Fatty acids, total saturated
-  645, //Fatty acids, total monounsaturated
-  646, //Fatty acids, total polyunsaturated
-];
 
 export default SearchPage;
