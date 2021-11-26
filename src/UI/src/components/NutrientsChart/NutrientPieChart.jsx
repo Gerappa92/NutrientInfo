6150ea802c44ee3bf67392b5668a9ac6bee9861f
Krzysztof Juszcze <Krzysztof_Juszcze@epam.com>
2021-11-26 13:22:34 +0100

    updated nutrient pie chart

--- src/UI/src/components/NutrientsChart/NutrientPieChart.jsx
+++ src/UI/src/components/NutrientsChart/NutrientPieChart.jsx
@@ -1,31 +1,53 @@
 import { getBasicNutrients } from "../../helpers/NutrientsHelper";
-import { PieChart, Pie, Cell, Tooltip, LabelList, Legend } from "recharts";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 export const NutrientPieChart = ({ nutrients }) => {
   let data = getBasicNutrients(nutrients);
+  data.forEach((n) => {
+    if (n.unitName === "MG") {
+      n.value = Math.round(n.value / 10) / 100;
+      n.unitName = "G";
+      n.valueWithUnit = `${n.value} g`;
+    }
+  });
+  const filteredData = data.filter((n) => n.unitName === "G");
 
   const COLORS = [
-    "#191716",
-    "#DAEFB3",
-    "#3D348B",
-    "#DB2B39",
-    "#3DA5D9",
-    "#EE964B",
-    "#EA638C",
-    "#5C8001",
-    "#EFEFEF",
+    { hex: "#3D348B", id: "307" }, //purple//sodium
+    { hex: "#DB2B39", id: "205" }, //red//carbohydrate
+    { hex: "#3DA5D9", id: "203" }, //blue//protein
+    { hex: "#EE964B", id: "291" }, //orange//fiber
+    { hex: "#D6D6D6", id: "269" }, //white//sugar
+    { hex: "#5C8001", id: "204" }, //Avocado//fat
+    { hex: "#191716", id: "-1" }, //default black
   ];
+
+  const getColor = (id) => {
+    const c = COLORS.find((c) => c.id === id);
+    if (!c) {
+      return COLORS.find((c) => c.id === "-1").hex;
+    }
+    return c.hex;
+  };
+
   return (
-    <>
-      <PieChart width={400} height={400}>
-        <Pie data={data} label>
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+    <ResponsiveContainer width="100%" height="100%">
+      <PieChart>
+        <Pie data={filteredData} label>
+          {filteredData.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={getColor(entry.id)} />
           ))}
         </Pie>
         <Legend dataKey="name" />
         <Tooltip />
       </PieChart>
-    </>
+    </ResponsiveContainer>
   );
 };
