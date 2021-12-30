3b444f730e1c62042bc828ae2b63f4f0c5b8d939
Krzysztof Juszcze <krzjus@gmail.com>
2021-12-30 14:13:17 +0100

    added contribution (#7)
    * fix integrations tests
    
    Co-authored-by: Krzysztof Juszcze <Krzysztof_Juszcze@epam.com>

--- test/Application.IntegrationTests/Food/Queries/SearchFoodTests.cs
+++ test/Application.IntegrationTests/Food/Queries/SearchFoodTests.cs
@@ -1,27 +1,34 @@
-﻿using Application.Food.Queries;
+﻿using Application.Common.Interfaces;
+using Application.Food.Queries;
+using Moq;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using static Application.IntegrationTests.Environment;
 
 namespace Application.IntegrationTests.Food.Queries
 {
     [TestFixture]
     public class SearchFoodTests
     {
+        private SearchFoodQueryHandler _searchFoodQueryHandler;
 
        [Test]
        public async Task ReturnAnyFoods()
         {
+            var foodDataServiceMock = new Mock<IFoodDataService>();
+            foodDataServiceMock.Setup(s => s.SearchFood(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<bool>()))
+                .ReturnsAsync(new Application.Food.Dto.SearchFoodDto()
+                {
+                    Foods = new Domain.Entities.Food[] {new Domain.Entities.Food() }
+                });
+            _searchFoodQueryHandler = new SearchFoodQueryHandler(foodDataServiceMock.Object);
+
             var query = new SearchFoodQuery()
             {
                 SearchTerm = "Banana"
             };
 
-            var result = await SendAsync(query);
+            var result = await _searchFoodQueryHandler.Handle(query, new System.Threading.CancellationToken());
 
             Assert.IsTrue(result.Foods.Any());
         }
