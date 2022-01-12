77a1d01cc7e42cf1546bb0405333fcae9202a5e7
Krzysztof Juszcze <Krzysztof_Juszcze@epam.com>
2022-01-12 16:21:13 +0100

    created meal creator form

--- src/UI/src/pages/MealCreator/MealCreatorPage.jsx
+++ src/UI/src/pages/MealCreator/MealCreatorPage.jsx
@@ -1,12 +1,21 @@
 import { Typography } from "antd";
+import { MealCreatorForm } from "../../components/MealCreatorForm/MealCreatorForm";
 import styled from "styled-components";
 
 const { Title } = Typography;
 
+const MealCreatorPageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0 20vw;
+`;
+
 export const MealCreatorPage = () => {
   return (
-    <>
+    <MealCreatorPageContainer>
       <Title>Meal Creator</Title>
-    </>
+      <MealCreatorForm></MealCreatorForm>
+    </MealCreatorPageContainer>
   );
 };
