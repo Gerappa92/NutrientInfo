a58a4f28b62ec7edaecb237b95a23ba7dd53f8c2
Krzysztof Juszcze <Krzysztof_Juszcze@epam.com>
2022-02-28 16:34:28 +0100

    updated food header

--- src/UI/src/components/FoodHeader/FoodHeader.jsx
+++ src/UI/src/components/FoodHeader/FoodHeader.jsx
@@ -1,16 +1,16 @@
 import { Space, Typography } from "antd";
 const { Title, Text } = Typography;
 
-export const FoodHeader = ({ food }) => {
+export const FoodHeader = ({ food, titleLevel, spaceDirection }) => {
   const getSource = () => {
     if (!food.brandOwner && !food.brandName) return food.dataSourceName;
     return "";
   };
   return (
     <>
-      <Title level={4}>{food.name}</Title>
+      <Title level={titleLevel ? titleLevel : 4}>{food.name}</Title>
 
-      <Space direction="vertical">
+      <Space direction={spaceDirection ? spaceDirection : "vertical"}>
         <Text type="secondary">{getSource()}</Text>
         <Text type="secondary">{food.brandOwner}</Text>
         <Text type="secondary">{food.brandName}</Text>
