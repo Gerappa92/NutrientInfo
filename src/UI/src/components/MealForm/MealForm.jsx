9f2b6ce955a23b387398ce2a162ce910b0dbf527
Krzysztof Juszcze <Krzysztof_Juszcze@epam.com>
2022-03-01 13:49:45 +0100

    created meal form

--- src/UI/src/components/MealForm/MealForm.jsx
+++ src/UI/src/components/MealForm/MealForm.jsx
@@ -1,40 +1,151 @@
-import { Button, Spin } from "antd";
-import { useState } from "react";
-import { List, Typography, Input, Pagination } from "antd";
+import React, { useState } from "react";
+import {
+  Form,
+  Input,
+  Button,
+  Space,
+  Spin,
+  List,
+  Pagination,
+  Typography,
+} from "antd";
 import { SearchFood } from "../SearchFood/SearchFood";
-import { IngridientListAddItem } from "../IngridientList/IngridientListAddItem";
-import { IngridientListItem } from "../IngridientList/IngridientListItem";
-import styled from "styled-components";
+import { FoodHeader } from "../FoodHeader/FoodHeader";
+import axios from "axios";
 
-export const MealForm = (props) => {
-  const defaultMeal = {
-    name: "",
-    ingridients: [],
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 10 },
+};
+
+const tailLayout = {
+  wrapperCol: { offset: 0, span: 24 },
+};
+
+const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+
+export const MealForm = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log(values);
+    const url = `${apiBaseUrl}mealmaker`;
+    // axios.post(url, values);
   };
-  const defaultIngridients = { foods: [], totalHits: 0 };
 
-  const [meal, setMeal] = useState(defaultMeal);
-  const [ingridients, setIngridients] = useState(defaultIngridients);
+  const validateIngridients = (_, ingridients) => {
+    if (!ingridients || ingridients.length < 1) {
+      return Promise.reject(new Error("At least 1 ingredient is required"));
+    }
+    return Promise.resolve();
+  };
+
+  const validateIngridientAmount = (_, value) => {
+    if (!value) {
+      return Promise.reject(new Error("An ingredient must selected"));
+    }
+    if (value.amount > 0) {
+      return Promise.resolve();
+    }
+    return Promise.reject(new Error("Amount must be grater than zero"));
+  };
+
+  return (
+    <Form {...layout} form={form} name="meal-form" onFinish={onFinish}>
+      <Form.Item
+        name="name"
+        label="Meal name"
+        rules={[
+          {
+            required: true,
+            message: "The name of the meal must be specified",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item name="description" label="Description">
+        <Input.TextArea rows={2} placeholder="This meal is perfect for..." />
+      </Form.Item>
+      <Form.List
+        name="ingridients"
+        rules={[
+          {
+            validator: validateIngridients,
+          },
+        ]}
+      >
+        {(fields, { add, remove }, { errors }) => (
+          <>
+            {fields.map(({ key, name, ...restField }, index) => (
+              <Form.Item
+                style={{ marginBottom: 5 }}
+                {...restField}
+                key={key}
+                name={[name, "ingridient"]}
+                label="Ingridient"
+                rules={[
+                  {
+                    validator: validateIngridientAmount,
+                  },
+                ]}
+              >
+                <IngridientInput
+                  hideSearch={index + 1 < fields.length}
+                  onRemove={() => remove(name)}
+                />
+              </Form.Item>
+            ))}
+            <Form.Item {...tailLayout}>
+              <Form.ErrorList errors={errors} />
+              <Button type="dashed" onClick={() => add()}>
+                Add Ingridient
+              </Button>
+            </Form.Item>
+          </>
+        )}
+      </Form.List>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+const IngridientInput = ({ value = {}, hideSearch, onChange, onRemove }) => {
+  const [ingridient, setIngridient] = useState({
+    id: 1,
+    name: "",
+    amount: null,
+  });
+
+  const [ingridients, setIngridients] = useState({ foods: [], totalHits: 0 });
   const [tableLoading, setTableLoading] = useState(false);
   const [pagination, setPagination] = useState({ pageNumber: 1, pageSize: 5 });
 
-  const addIngridient = (ingriditent) => {
-    console.log(ingriditent);
-    setMeal((prevMeal) => {
-      return {
-        ...prevMeal,
-        ingridients: [...prevMeal.ingridients, ingriditent],
-      };
+  const triggerChange = (changedValue) => {
+    onChange?.({
+      id: ingridient.id,
+      name: ingridient.name,
+      amount: ingridient.amount,
+      ...value,
+      ...changedValue,
     });
   };
 
-  const removeIngridient = (ingridient) => {
-    setMeal((prevMeal) => {
-      return {
-        ...prevMeal,
-        ingridients: prevMeal.ingridients.filter((i) => i.id !== ingridient.id),
-      };
-    });
+  const handleOnChange = (event) => {
+    const { value, name } = event.target;
+    setIngridient((prevIngridient) => ({ ...prevIngridient, [name]: value }));
+    triggerChange({ [name]: value });
+  };
+
+  const addIngridient = (ingridient) => {
+    setIngridient(ingridient);
+    triggerChange(ingridient);
+    setIngridients({ foods: [], totalHits: 0 });
   };
 
   const onPageChange = (pageNumber) => {
@@ -46,73 +157,73 @@ export const MealForm = (props) => {
   };
 
   return (
-    <FormDiv>
-      <div>
-        <Typography.Text>Meal name:</Typography.Text>
+    <>
+      <Space>
         <Input
+          name="id"
           type="text"
-          style={{ maxWidth: "200px", marginLeft: "10px" }}
-          onChange={(e) =>
-            setMeal((prevMeal) => ({ ...prevMeal, name: e.target.value }))
-          }
+          value={value.id || ingridient.id}
+          onChange={handleOnChange}
+          disabled={true}
+          hidden
         />
-      </div>
-      <div>
-        <List
-          style={{ marginTop: "10px", maxWidth: "800px" }}
-          header={<Typography.Title level={5}>Ingridients</Typography.Title>}
-          footer={
-            <>
-              <SearchFood
-                setData={setIngridients}
-                setTableLoading={setTableLoading}
-                pageNumber={pagination.pageNumber}
-                pageSize={pagination.pageSize}
-              />
-              <Spin spinning={tableLoading} size="large">
-                {ingridients.foods.length > 0 && (
-                  <>
-                    <List
-                      bordered
-                      dataSource={ingridients.foods}
-                      renderItem={(item) => (
-                        <IngridientListAddItem
-                          ingridient={item}
-                          handleClick={addIngridient}
-                        />
-                      )}
-                    ></List>
-                    <Pagination
-                      current={pagination.pageNumber}
-                      total={ingridients.totalHits}
-                      hideOnSinglePage={true}
-                      onChange={onPageChange}
-                      onShowSizeChange={onShowSizeChange}
-                    />
-                  </>
-                )}
-              </Spin>
-            </>
-          }
-          bordered
-          dataSource={meal.ingridients}
-          renderItem={(item) => (
-            <IngridientListItem
-              ingridient={item}
-              handleClick={removeIngridient}
-            />
-          )}
-        ></List>
-      </div>
-      <Button style={{ margin: "10px", maxWidth: "200px" }} type="primary">
-        Create
-      </Button>
-    </FormDiv>
+        <Input
+          name="name"
+          type="text"
+          value={value.name || ingridient.name}
+          onChange={handleOnChange}
+          placeholder="name"
+          disabled={true}
+        />
+        <Input
+          name="amount"
+          type="number"
+          value={value.amount || ingridient.amount}
+          onChange={handleOnChange}
+          placeholder="amount"
+        />
+        <Typography.Text type="secondary">g</Typography.Text>
+        <Button danger type="dashed" onClick={onRemove}>
+          Remove
+        </Button>
+      </Space>
+      {!hideSearch && (
+        <>
+          <SearchFood
+            setData={setIngridients}
+            setTableLoading={setTableLoading}
+            pageNumber={pagination.pageNumber}
+            pageSize={pagination.pageSize}
+          />
+          <Spin spinning={tableLoading} size="large">
+            {ingridients.foods.length > 0 && (
+              <>
+                <List
+                  bordered
+                  dataSource={ingridients.foods}
+                  renderItem={(item) => (
+                    <List.Item>
+                      <FoodHeader
+                        food={item}
+                        titleLevel={5}
+                        spaceDirection="horizontal"
+                      />
+                      <Button onClick={() => addIngridient(item)}>Add</Button>
+                    </List.Item>
+                  )}
+                ></List>
+                <Pagination
+                  current={pagination.pageNumber}
+                  total={ingridients.totalHits}
+                  hideOnSinglePage={true}
+                  onChange={onPageChange}
+                  onShowSizeChange={onShowSizeChange}
+                />
+              </>
+            )}
+          </Spin>
+        </>
+      )}
+    </>
   );
 };
-
-const FormDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
--- src/UI/src/pages/NewMealPage/NewMealPage.jsx
+++ src/UI/src/pages/NewMealPage/NewMealPage.jsx
@@ -1,7 +1,6 @@
 import { Typography } from "antd";
 import { MealForm } from "../../components/MealForm/MealForm";
 import styled from "styled-components";
-import { useState } from "react";
 
 const { Title } = Typography;
 
@@ -13,24 +12,10 @@ const MealCreatorPageContainer = styled.div`
 `;
 
 export const NewMealPage = () => {
-  // const [meal, setMeal] = useState();
-  // const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
-
-  // useEffect(() => {
-  //   console.info(meal);
-  //   postMeal();
-  // }, [meal]);
-
-  // const postMeal = () => {
-  //   const url = `${apiBaseUrl}mealmaker`;
-  //   axios.post(url, meal);
-  // };
-
   return (
     <MealCreatorPageContainer>
       <Title>Meal Creator</Title>
-      {/* <MealCreatorForm setMeal={setMeal}></MealCreatorForm> */}
-      <MealForm></MealForm>
+      <MealForm />
     </MealCreatorPageContainer>
   );
 };
