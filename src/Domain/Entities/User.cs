bed669d73d75fce2c4b821a9d98633d65b6bd786
Krzysztof Juszcze <Krzysztof_Juszcze@epam.com>
2022-03-02 15:07:23 +0100

    updated User domain entity

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -1,14 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
     public class User
     {
-        public string Id { get; set; }
+        public User(string email, string name, string password)
+        {
+            Email = email;
+            Name = string.IsNullOrEmpty(name) ? email : name;
+            Password = password;
+        }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string Password { get; set; }
+        public string AuthenticationType { get; set; }
+        public bool IsAuthenticated { get; set; }
         public string Name { get; set; }
     }
 }
