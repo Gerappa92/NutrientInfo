734f11b6dfb0e898b8fb43485f4c7fe8af9c8876
Krzysztof Juszcze <Krzysztof_Juszcze@epam.com>
2022-03-30 10:42:29 +0200

    updated error message

--- src/UI/src/components/ErrorMessage/ErrorMessage.jsx
+++ src/UI/src/components/ErrorMessage/ErrorMessage.jsx
@@ -2,11 +2,13 @@ import { Alert } from "antd";
 
 export const ErrorMessage = ({ error }) => {
   return (
-    <div hidden={!(error && error.response && error.response.data.Message)}>
-      <Alert
-        message={error && error.response && error.response.data.Message}
-        type="error"
-      />
-    </div>
+    <>
+      {error && error.response && error.response.data.Message && (
+        <Alert
+          message={error && error.response && error.response.data.Message}
+          type="error"
+        />
+      )}
+    </>
   );
 };
